204a8ee824b89a60388cc2cac67185ee6ae1e9b1
Mahbubul Hasan <39461684+shaon11579@users.noreply.github.com>
2021-08-11 08:48:18 -0500

    Update what to do with a new data? R  code.R

--- what to do with a new data? R  code.R
+++ what to do with a new data? R  code.R
@@ -1,18 +1,24 @@
+#set dir 
 setwd("~/OneDrive/R_Dir")
+
+# use spss format .sav file ..use memisc lib
 install.packages("memisc")
 library(memisc)
 library(lattice)
 library(MASS)
+# read data and save it in csv file to make your life easy 
 RISE <- data.frame(as.data.set(spss.system.file("RISEItemCombined.sav")))
 dir()
 RISE<- dataset
 write.csv(dataset, "~/OneDrive/R_Dir/dataset.csv")
-
+# read data and explore data summary 
 SARA <- data.frame(as.data.set(spss.system.file("SARA_combined.sav")))
 write.csv(SARA,"~/OneDrive/R_Dir/SARA.csv")
 summary(dataset)
 str(dataset)
 
+# read data and explore data summary 
+# try three things summary, head, tail and str funtions 
 summary(SARA)
 head(SARA,10)
 tail(SARA)
